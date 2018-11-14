4cc120de36de0a9d333f0e5fb9f345d14e6bea8d
ChenglinLu <chenglinlu@jlq.com>
2018-11-14 16:35:08 +0800

    [update]ref link in main file

--- simpleOccupancy_main.c
+++ simpleOccupancy_main.c
@@ -1,4 +1,9 @@
-//Ref:https://blog.csdn.net/bendanban/article/details/8518382
+/***************************************************************
+Ref:
+1. https://blog.csdn.net/bendanban/article/details/8518382
+2. https://devblogs.nvidia.com/cuda-pro-tip-occupancy-api-simplifies-launch-configuration/
+***************************************************************/
+
 #include "stdio.h"
 
 void launchMyKernel();
